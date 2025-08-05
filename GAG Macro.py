96ea768bcb909377623e6ca45c2182a0c10a6f5e
Mr-Bored-Board <strongsyed@gmail.com>
2025-08-05 05:37:07 -0500

    Added macro versio checker

--- GAG Macro.py
+++ GAG Macro.py
@@ -1,7 +1,38 @@
-import sys, re, requests
+import sys, re, requests, json, appdirs
 from packaging import version
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QHBoxLayout, QVBoxLayout, QWidget, QPushButton
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QHBoxLayout, QVBoxLayout, QWidget, QPushButton, QMessageBox
 from PyQt5.QtCore import Qt
+from appdirs import user_data_dir
+
+
+
+current_macro_version = "0.0.0.1"
+macro_requirements_url = "https://raw.githubusercontent.com/Mr-Bored-Board/Grow-A-Garden-Macro/main/Macro%20Requirements.json"
+github_release_url = "https://api.github.com/repos/Mr-Bored-Board/Grow-A-Garden-Macro/releases/latest"
+latest_macro_version = None
+
+# make the script tell the user what went wrong if something happened
+def check_for_updates():
+    try:
+        response = requests.get(macro_requirements_url, timeout=5)
+        if response.status_code == 200:
+            config = json.loads(response.text)
+            macro_info = config["Macro Requirements"]["latest_macro_version"]
+            print(macro_info)
+            global latest_macro_version
+            latest_macro_version = macro_info.get("Latest Macro Version", None)
+            if latest_macro_version:
+                return version.parse(latest_macro_version) > version.parse(current_macro_version)
+            else:
+                print("Could not find 'Latest Macro Version' in config.")
+                return False
+        else:
+            print("Failed to fetch requirements.json (HTTP {})".format(response.status_code))
+            return False
+    except Exception as e:
+        QMessageBox.information(None, "Error", f"Failed to check for updates: {str(e)}")
+    return False
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -28,7 +59,7 @@ class MainWindow(QMainWindow):
         self.setObjectName("MainWindow")
         self.status_label.setObjectName("Status_Label")
         self.setStyleSheet("""QMainWindow {background-color: black; } QPushButton {background-color: black; color: cyan; border-radius: 5px; border: 1px solid cyan; font-size: 30px;} QLabel {color: cyan; font-size: 50px;}""")
-        self.status_label.setAlignment(Qt.AlignCenter)
+        self.status_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
 
 
@@ -37,6 +68,7 @@ class MainWindow(QMainWindow):
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    check_for_updates()
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
